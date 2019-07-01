7f2d7d6cbb0190d882cf11041aff355ddaded9a5
Bruno Sacaric <bruno.sacaric@fer.hr>
2019-07-01 22:43:09 +0200

    app commit

---
+++ app/src/main/java/com/infinum/shows_bruno_sacaric/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.infinum.shows_bruno_sacaric
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
