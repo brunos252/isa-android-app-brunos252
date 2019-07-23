8ee55c7d9c13138ca0c61b80b4ece5cf299cd3a0
Bruno Sacaric <bruno.sacaric@fer.hr>
2019-07-23 22:32:31 +0200

    save before checkout

---
+++ app/src/main/java/com/infinum/shows_bruno_sacaric/ShowsContainerActivity.kt
@@ -0,0 +1,12 @@
+package com.infinum.shows_bruno_sacaric
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowsContainerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shows_container)
+    }
+}
